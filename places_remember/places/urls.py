30b765fc57082da3d44295a855f54cf123f20fbb
lomaxwes <lomaxwes@gmail.com>
2023-05-18 15:53:15 +0500

    urls home, login and logout

--- places_remember/places/urls.py
+++ places_remember/places/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('account/login-success', views.login_success, name='login_success'),
-    path('logout/', views.logout_view, name='logout'),
+    path('', views.home, name='home'),  # greeting
+    path('account/login-success', views.login_success, name='login_success'),  # authentication
+    path('logout/', views.logout_view, name='logout'),  # logout
 ]
