b031c6807b1e4b5ee3502c34d6c9657af33266ee
lomaxwes <lomaxwes@gmail.com>
2023-05-18 10:54:46 +0500

    Remove google_login.html from index

---
+++ places_remember/places_remember/secrets.py
@@ -0,0 +1,4 @@
+
+CLIENT_ID = '683152318873-0gt2b7j52b07o7e9ep5ct15ehkomtv0c.apps.googleusercontent.com'
+SECRET = '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'
+DJANGO_KEY = 'django-insecure-1hxtj-9rcm(vxbken)mu+jgrr8375hutpy0yr2gl=_0-65=va1'
