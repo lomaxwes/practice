1e6e16abb6fff4f7031dd79536ecc2d08b3135b1
lomaxwes <lomaxwes@gmail.com>
2023-05-22 10:38:53 +0500

    corrected comments

--- places_remember/places/urls.py
+++ places_remember/places/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.home, name='home'),  # greeting
-    path('account/login-success', views.login_success, name='login_success'),  # authentication
+    path('account/login-success', views.login_success, name='login_success'),  # user is authenticated
     path('logout/', views.logout_view, name='logout'),  # logout
     path('add_memory/', views.add_memory, name='add_memory'),  # add_memory
 ]
