dfc4f7f833bbdebc8c6ff0f7dd2135ace39130b0
lomaxwes <lomaxwes@gmail.com>
2023-05-23 14:43:36 +0500

    tests with location

--- places_remember/places/tests.py
+++ places_remember/places/tests.py
@@ -3,34 +3,26 @@ from django.contrib.auth.models import User
 from .models import Place
 
 
-class PlaceTestCase(TestCase):
-    def setUp(self):
-        # Creating a test user
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
+class PlaceTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Creating a user to connect to a place
+        cls.user = User.objects.create(username='testuser')
 
-    def test_create_place(self):
-        # Creating a new impression of the place visited
-        place = Place.objects.create(user=self.user, name='Test Place', comment='Test Comment')
+        # Creating a test location
+        cls.place = Place.objects.create(
+            user=cls.user,
+            name='Test Place',
+            comment='This is a test place',
+            location='Test Location'
+        )
 
-        # Check that the impression has been successfully created
-        self.assertEqual(place.name, 'Test Place')
-        self.assertEqual(place.comment, 'Test Comment')
-        self.assertEqual(place.user, self.user)
+    def test_str_representation(self):
+        self.assertEqual(str(self.place), 'testuser')
 
-    def test_get_place_list(self):
-        # Creating a few impressions about the places visited
-        Place.objects.create(user=self.user, name='Place 1', comment='Comment 1')
-        Place.objects.create(user=self.user, name='Place 2', comment='Comment 2')
-
-        # We get a list of impressions about the places visited
-        places = Place.objects.filter(user=self.user)
-
-        # Check that the list contains the expected number of items
-        self.assertEqual(places.count(), 2)
-
-        # Check that the list items match the expected values
-        self.assertEqual(places[0].name, 'Place 1')
-        self.assertEqual(places[0].comment, 'Comment 1')
-        self.assertEqual(places[1].name, 'Place 2')
-        self.assertEqual(places[1].comment, 'Comment 2')
+    def test_place_attributes(self):
+        self.assertEqual(self.place.user, self.user)
+        self.assertEqual(self.place.name, 'Test Place')
+        self.assertEqual(self.place.comment, 'This is a test place')
+        self.assertEqual(self.place.location, 'Test Location')
 
