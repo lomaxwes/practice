1f06f6df9628eaf4397754ce376cb7a2c6d7a308
lomaxwes <lomaxwes@gmail.com>
2023-05-23 14:29:22 +0500

    registration of the Place model

--- places_remember/places/admin.py
+++ places_remember/places/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import UserProfile
+from .models import UserProfile, Place
 
 # Register your models here.
 
 admin.site.register(UserProfile)
+admin.site.register(Place)
