13540d82501b43a724f74e17b68f0b1f7714d5a0
lomaxwes <lomaxwes@gmail.com>
2023-05-23 14:23:30 +0500

    added labels

--- places_remember/places/forms.py
+++ places_remember/places/forms.py
@@ -1,8 +1,15 @@
 from django import forms
+from mapbox_location_field.forms import LocationField
+from django.core.validators import MinLengthValidator
+
 from .models import Place
 
 
 class MemoryForm(forms.ModelForm):
     class Meta:
         model = Place
-        fields = ['name', 'comment']
+        fields = ['name', 'comment', 'location']
+        labels = {
+            'name': 'Place name:',
+            'comment': 'Description for this place:'
+        }
