d308dc642195c93bd43733231551627d29ecb27c
lomaxwes <lomaxwes@gmail.com>
2023-05-23 15:53:47 +0500

    added validation

--- places_remember/places/forms.py
+++ places_remember/places/forms.py
@@ -6,6 +6,10 @@ from .models import Place
 
 
 class MemoryForm(forms.ModelForm):
+    name = forms.CharField(max_length=100, validators=[MinLengthValidator(3)], required=True)
+    comment = forms.CharField(widget=forms.Textarea, required=True)
+    location = forms.CharField(required=True)
+
     class Meta:
         model = Place
         fields = ['name', 'comment', 'location']
